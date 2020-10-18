694bf66c55cf53e8ba91a7c6f0b2c9312b78e4a0
Ivan Bachyla <ivbachilo@gmail.com>
2020-10-18 18:38:17 +0200

    Conduit server - initial commit

---
+++ conduit-server/src/test/java/com/github/ibachyla/conduit/ConduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ibachyla.conduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConduitApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
